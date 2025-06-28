919595e7dc35cae132ecab2f72b029d8ec13db13
Serhii Yatsenko <serhii2111@yahoo.com>
2025-06-28 15:52:09 +0300

    one_comment

--- task01.py
+++ task01.py
@@ -1,20 +1,21 @@
 
 def caching_fibonacci(): 
-    cache = {}
+    cache = {}    # Локальна змінна зовнішньої функції
 
     def fibonacci(n):
         if n <= 0:
-            return 0
+            return 0 # Базовий випадок: 0-те число Фібоначчі
         if n == 1:
-            return 1         
-        
+            return 1 # Базовий випадок: 1-ше число Фібоначчі     
+        # Використовує змінну cache із зовнішньої області видимості
         if n in cache:
-            return cache[n]
+            return cache[n]  # Повертаємо кешоване значення, у разі наявності
         
+        # Якщо немає в кеші — обчислюємо рекурсивно та зберігаємо в кеш
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return cache[n]      
     
-    return fibonacci
+    return fibonacci # Повертаємо внутрішню функцію 
  
 
 fib = caching_fibonacci()
